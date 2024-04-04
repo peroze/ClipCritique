6deb955512bae912f98b8e471bbe5e707cc02140
Athanasios Efstathiou <145462138+a-efstathiou@users.noreply.github.com>
2024-04-04 21:36:09 +0300

    Add files via upload

---
+++ ClipCritique/src/main/java/com/unipi/software/tech/ClipCritique/ClipCritiqueApplication.java
@@ -0,0 +1,13 @@
+package com.unipi.software.tech.ClipCritique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClipCritiqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClipCritiqueApplication.class, args);
+	}
+
+}
